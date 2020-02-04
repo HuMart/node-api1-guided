e748ba354ee886cfa02568467e86df52090fb02e
HuMart <manya01@protonmail.com>
2020-02-04 14:40:57 -0500

    delete method

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const Hubs = require('./data/hubs-model.js'); // our Hubs database Library
  
 const server = express();
 
+// middleware: teaches express new thiings
+server.use(express.json()); // needed to parse json
+
 // routes or endpoints
+
 // Get to "/"
 
 server.get('/', function(request, response) {
     response.send({hello: 'World!!'});
 });
+
 // see a list of hub
 server.get('/api/hubs', (req, res) => {
     // read the data from the database
@@ -25,11 +30,43 @@ server.get('/api/hubs', (req, res) => {
             res.status(500).json({errorMessage: "sorry, you run into an error getting the list of hubs"})
         })
 })
+
 // create a hub
+
+server.post('/api/hubs', (req, res) => {
+    const hubData = req.body;
+    // never trust the client, validate data.
+    Hubs.add(hubData)
+        .then(hub => {
+        res.status(201).json(hub);
+    })
+        .catch(error => {
+            console.log(error);
+            // handle error
+            res.status(500).json({errorMessage: "sorry, we run into an error posting the new data of hubs"})
+    })
+})
 
 // delete a hub
 
+server.delete('/api/hubs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Hubs.remove(id)
+        .then(deleted => {
+            // res.status(204).end();
+            res.status(200).json(deleted);
+        })
+            .catch(error => {
+                console.log(error);
+                // handle error
+                res.status(500).json({errorMessage: "sorry, we run into an error removing data of hubs"})
+        })
+
+})
+
 // update a hub
+
 const port = 8000;
 server.listen(port, () => console.log(`\n *** api on port: ${port} ***\n`));
 
